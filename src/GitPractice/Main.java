57df547a95b78aeaba0740500159fc17808f1128
Zhonaryar <zabih.honaryar@gmail.com>
2025-03-14 11:14:48 -0400

    Second Commit

---
+++ src/GitPractice/Main.java
@@ -0,0 +1,11 @@
+package GitPractice;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This is in main branch");
+	}
+
+}
